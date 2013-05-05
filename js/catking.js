eb1cb000f152314743b2080ac2c89f976508ff1b
Jacob Stanley <jacob@stanley.io>
2013-05-05 15:18:34 +0800

    Switched to using Ammo.js directly (instead of Physijs)

--- js/catking.js
+++ js/catking.js
@@ -1,17 +1,22 @@
 (function(){
-  var vec3, scene, renderer, camera, rig, rigVolume, gravity, render, update;
+  var a3, t3, scene, renderer, camera, world, rigThree, rigAmmo, rigVolume, floatThree, render, update, logged, simulateBuoyancy, updateScene;
   import$(this, prelude);
-  vec3 = function(x, y, z){
+  a3 = function(x, y, z){
+    return new Ammo.btVector3(x, y, z);
+  };
+  t3 = function(x, y, z){
     return new THREE.Vector3(x, y, z);
   };
   scene = null;
   renderer = null;
   camera = null;
-  rig = null;
-  rigVolume = 0;
-  gravity = vec3(0, 0, -9.8);
+  world = null;
+  rigThree = null;
+  rigAmmo = null;
+  rigVolume = null;
+  floatThree = null;
   window.onload = function(){
-    var width, height, x$, y$, near, far, zoom, left, top, deg, z$, floorG, floorM, z1$, floor, seaG, seaM, sea, rigG, z2$, rigM, z3$, z4$, dir, amb;
+    var width, height, x$, y$, z$, collisionCfg, dispatcher, broadphase, solver, near, far, zoom, left, top, z1$, floorG, floorM, z2$, floorShape, z3$, floorTransform, floorMass, floorInertia, floorMotionState, floorRbInfo, floorAmmo, oceanG, oceanM, z4$, floatG, z5$, floatM, rigG, z6$, rigM, z7$, rigMass, z8$, rigRotation, z9$, rigTransform, rigInertia, rigShape, rigMotionState, rigRbInfo, z10$;
     width = window.innerWidth;
     height = window.innerHeight;
     x$ = renderer = new THREE.WebGLRenderer({
@@ -19,76 +24,143 @@
     });
     x$.setSize(width, height);
     x$.shadowMapEnabled = true;
-    x$.shadowMapSoft = true;
+    x$.shadowMapSoft = false;
     document.getElementById('viewport').appendChild(renderer.domElement);
-    y$ = scene = new Physijs.Scene;
-    y$.setGravity(gravity);
-    y$.addEventListener('update', update);
+    scene = new THREE.Scene;
+    y$ = new THREE.DirectionalLight(0xffffff);
+    y$.position.set(-300, 300, 500);
+    y$.castShadow = true;
+    scene.add(
+    y$);
+    z$ = new THREE.AmbientLight(0x777777);
+    scene.add(
+    z$);
+    collisionCfg = new Ammo.btDefaultCollisionConfiguration;
+    dispatcher = new Ammo.btCollisionDispatcher(collisionCfg);
+    broadphase = new Ammo.btDbvtBroadphase();
+    solver = new Ammo.btSequentialImpulseConstraintSolver();
+    world = new Ammo.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionCfg);
+    world.setGravity(new Ammo.btVector3(0, 0, -9.8));
     near = 1;
     far = 1000;
     zoom = 0.5;
     left = -width * 0.5 * zoom;
     top = height * 0.5 * zoom;
-    deg = (function(it){
-      return it * 0.0174532925;
-    });
-    z$ = camera = new THREE.OrthographicCamera(left, -left, top, -top, near, far);
-    z$.up = vec3(0, 0, 1);
-    z$.position.set(-300, -300, 245);
-    z$.lookAt(vec3(0, 0, 0));
-    scene.add(camera);
+    z1$ = camera = new THREE.OrthographicCamera(left, -left, top, -top, near, far);
+    z1$.up = t3(0, 0, 1);
+    z1$.position.set(-300, -300, 245);
+    z1$.lookAt(t3(0, 0, 0));
+    scene.add(
+    z1$);
     floorG = new THREE.PlaneGeometry(200, 200);
     floorM = new THREE.MeshBasicMaterial({
       color: 0x333300
     });
-    z1$ = floor = new Physijs.PlaneMesh(floorG, floorM, 0);
-    z1$.receiveShadow = true;
-    z1$.position.set(0, 0, -100);
-    scene.add(floor);
-    seaG = new THREE.PlaneGeometry(200, 200);
-    seaM = new THREE.MeshBasicMaterial({
+    z2$ = new THREE.Mesh(floorG, floorM);
+    z2$.receiveShadow = true;
+    z2$.position.set(0, 0, -100);
+    scene.add(
+    z2$);
+    floorShape = new Ammo.btStaticPlaneShape(a3(0, 0, 1), 0);
+    z3$ = floorTransform = new Ammo.btTransform();
+    z3$.setIdentity();
+    z3$.setOrigin(a3(0, 0, -100));
+    floorMass = 0;
+    floorInertia = a3(0, 0, 0);
+    floorMotionState = new Ammo.btDefaultMotionState(floorTransform);
+    floorRbInfo = new Ammo.btRigidBodyConstructionInfo(floorMass, floorMotionState, floorShape, floorInertia);
+    floorAmmo = new Ammo.btRigidBody(floorRbInfo);
+    world.addRigidBody(floorAmmo);
+    oceanG = new THREE.PlaneGeometry(200, 200);
+    oceanM = new THREE.MeshBasicMaterial({
       color: 0x112244,
       transparent: true,
       opacity: 0.8
     });
-    sea = new THREE.Mesh(seaG, seaM);
-    scene.add(sea);
-    rigVolume = 100 * 100 * 15;
+    z4$ = new THREE.Mesh(oceanG, oceanM);
+    z4$.receiveShadow = true;
+    scene.add(
+    z4$);
+    floatG = new THREE.CubeGeometry(5, 5, 25);
+    z5$ = floatM = new THREE.MeshLambertMaterial({
+      color: 0xff0000
+    });
+    z5$.ambient = z5$.color;
+    floatThree = new THREE.Mesh(floatG, floatM);
     rigG = new THREE.CubeGeometry(100, 100, 15);
-    z2$ = rigM = new THREE.MeshLambertMaterial({
+    z6$ = rigM = new THREE.MeshLambertMaterial({
       color: 0x335533
     });
-    z2$.ambient = z2$.color;
-    z3$ = rig = new Physijs.BoxMesh(rigG, rigM, rigVolume * 500);
-    z3$.castShadow = true;
-    z3$.position.z = 100;
-    scene.add(rig);
-    rig.setDamping(0.3, 0.3);
-    z4$ = dir = new THREE.DirectionalLight(0xffffff);
-    z4$.position.set(-1, 0, 2);
-    z4$.castShadow = true;
-    scene.add(dir);
-    amb = new THREE.AmbientLight(0x777777);
-    scene.add(amb);
+    z6$.ambient = z6$.color;
+    z7$ = rigThree = new THREE.Mesh(rigG, rigM);
+    z7$.castShadow = true;
+    z7$.receiveShadow = true;
+    z7$.useQuaternion = true;
+    z7$.position.z = 100;
+    scene.add(
+    z7$);
+    rigVolume = 100 * 100 * 15;
+    rigMass = rigVolume * 700;
+    z8$ = rigRotation = new Ammo.btQuaternion();
+    z8$.setEuler(-pi / 8.0, pi / 16.0, 0);
+    z9$ = rigTransform = new Ammo.btTransform();
+    z9$.setIdentity();
+    z9$.setOrigin(a3(0, 0, 100));
+    z9$.setRotation(rigRotation);
+    rigInertia = a3(0, 0, 0);
+    rigShape = new Ammo.btBoxShape(a3(100, 100, 15));
+    rigShape.calculateLocalInertia(rigMass, rigInertia);
+    rigMotionState = new Ammo.btDefaultMotionState(rigTransform);
+    rigRbInfo = new Ammo.btRigidBodyConstructionInfo(rigMass, rigMotionState, rigShape, rigInertia);
+    z10$ = rigAmmo = new Ammo.btRigidBody(rigRbInfo);
+    z10$.setDamping(0.2, 0.2);
+    world.addRigidBody(rigAmmo);
     return requestAnimationFrame(render);
   };
   render = function(){
-    scene.simulate(undefined, 1);
+    update();
     renderer.render(scene, camera);
     return requestAnimationFrame(render);
   };
-  update = function(x, y, z){
-    var rb, bs, offset, submerged, fluidDensity, volumeDisplaced, buoyancy;
-    rb = rig;
-    bs = rb.geometry.boundingSphere;
-    offset = 15 - (7.5 + min(7.5, max(-7.5, rb.position.z)));
-    submerged = offset / 15;
-    if (submerged > 0) {
+  update = function(){
+    simulateBuoyancy();
+    world.stepSimulation(1 / 60, 5);
+    return updateScene();
+  };
+  logged = false;
+  simulateBuoyancy = function(){
+    var t, xy, floats, tf0, i$, len$, f, tf, fluidDensity, height, hheight, offset, volumeDisplaced, gravity, buoyancy, results$ = [];
+    t = new Ammo.btTransform();
+    rigAmmo.getMotionState().getWorldTransform(t);
+    xy = 50 - 7.5;
+    floats = [a3(xy, xy, 0), a3(xy, -xy, 0), a3(-xy, xy, 0), a3(-xy, -xy, 0)];
+    tf0 = t.op_mul(floats[3]);
+    floatThree.position.set(tf0.x(), tf0.y(), tf0.z());
+    for (i$ = 0, len$ = floats.length; i$ < len$; ++i$) {
+      f = floats[i$];
+      tf = t.op_mul(f);
       fluidDensity = 1000;
-      volumeDisplaced = rigVolume * submerged;
-      buoyancy = gravity.clone().negate().multiplyScalar(fluidDensity * volumeDisplaced);
-      return rb.applyCentralForce(buoyancy);
+      height = 15;
+      hheight = height / 2.0;
+      offset = height - (hheight + min(hheight, max(-hheight, tf.z())));
+      volumeDisplaced = (rigVolume / floats.length) * (offset / hheight);
+      if (volumeDisplaced <= 0) {
+        continue;
+      }
+      gravity = -9.8;
+      buoyancy = a3(0, 0, -gravity * fluidDensity * volumeDisplaced);
+      results$.push(rigAmmo.applyForce(buoyancy, f));
     }
+    return results$;
+  };
+  updateScene = function(){
+    var t, pos, rot;
+    t = new Ammo.btTransform();
+    rigAmmo.getMotionState().getWorldTransform(t);
+    pos = t.getOrigin();
+    rot = t.getRotation();
+    rigThree.position.set(pos.x(), pos.y(), pos.z());
+    return rigThree.quaternion.set(rot.x(), rot.y(), rot.z(), rot.w());
   };
   function import$(obj, src){
     var own = {}.hasOwnProperty;
--- js/catking.ls
+++ js/catking.ls
@@ -1,30 +1,58 @@
 import prelude
 
-vec3 = (x, y, z) -> new THREE.Vector3 x, y, z
+a3 = (x, y, z) -> new Ammo.bt-vector3 x, y, z
+t3 = (x, y, z) -> new THREE.Vector3 x, y, z
 
 scene    = null
 renderer = null
 camera   = null
 
-rig = null
-rig-volume = 0
+world    = null
 
-gravity = vec3 0, 0, -9.8
+rig-three  = null
+rig-ammo   = null
+rig-volume = null
+
+float-three = null
 
 window.onload = ->
   width  = window.inner-width
   height = window.inner-height
 
+  # Renderer
   renderer := new THREE.WebGLRenderer antialias: false
-    ..setSize width, height
+    ..set-size width, height
     ..shadow-map-enabled = true
-    ..shadow-map-soft = true
+    ..shadow-map-soft = false
+  document
+    .get-element-by-id \viewport
+    .append-child renderer.dom-element
 
-  document.getElementById 'viewport' .appendChild renderer.domElement
+  # Scene
+  scene := new THREE.Scene
 
-  scene := new Physijs.Scene
-    ..set-gravity gravity
-    ..add-event-listener \update, update
+  # Lights
+  new THREE.DirectionalLight 0xffffff
+    #..shadow-camera-visible = true
+    #..shadow-camera-left    = -150
+    #..shadow-camera-right   = 150
+    #..shadow-camera-top     = 150
+    #..shadow-camera-bottom  = -150
+    ..position.set -300, 300, 500
+    ..cast-shadow = true
+    .. |> scene.add
+
+  new THREE.AmbientLight 0x777777
+    .. |> scene.add
+
+  # Ammo
+  collision-cfg = new Ammo.bt-default-collision-configuration
+  dispatcher    = new Ammo.bt-collision-dispatcher collision-cfg
+  broadphase    = new Ammo.bt-dbvt-broadphase!
+  solver        = new Ammo.bt-sequential-impulse-constraint-solver!
+
+  world := new Ammo.bt-discrete-dynamics-world dispatcher, broadphase, solver, collision-cfg
+  world.set-gravity new Ammo.bt-vector3 0, 0, -9.8
 
   # Camera
   near = 1
@@ -33,71 +61,124 @@ window.onload = ->
   left = -width * 0.5 * zoom
   top  = height * 0.5 * zoom
 
-  deg = (* 0.0174532925)
-
-  #camera := new THREE.PerspectiveCamera fov, aspect, near, far
   camera := new THREE.OrthographicCamera left, -left, top, -top, near, far
-      ..up = vec3 0, 0, 1
+      ..up = t3 0, 0, 1
       ..position.set -300, -300, 245
-      ..lookAt vec3 0, 0, 0
-
-  scene.add camera
+      ..lookAt t3 0, 0, 0
+      .. |> scene.add
 
   # Floor
   floor-g = new THREE.PlaneGeometry 200, 200
   floor-m = new THREE.MeshBasicMaterial color: 0x333300
-  floor = new Physijs.PlaneMesh floor-g, floor-m, 0
+  new THREE.Mesh floor-g, floor-m
     ..receive-shadow = true
     ..position.set 0, 0, -100
-  scene.add floor
+    .. |> scene.add
 
-  sea-g = new THREE.PlaneGeometry 200, 200
-  sea-m = new THREE.MeshBasicMaterial color: 0x112244, transparent: true, opacity: 0.8
-  sea = new THREE.Mesh sea-g, sea-m
-  scene.add sea
+  floor-shape = new Ammo.bt-static-plane-shape (a3 0, 0, 1), 0
+  floor-transform = new Ammo.bt-transform!
+    ..set-identity!
+    ..set-origin a3 0, 0, -100
+  floor-mass = 0
+  floor-inertia = a3 0, 0, 0
+  floor-motion-state = new Ammo.bt-default-motion-state floor-transform
+  floor-rb-info = new Ammo.bt-rigid-body-construction-info floor-mass, floor-motion-state, floor-shape, floor-inertia
+  floor-ammo = new Ammo.bt-rigid-body floor-rb-info
+  world.add-rigid-body floor-ammo
+
+  # Ocean
+  ocean-g = new THREE.PlaneGeometry 200, 200
+  ocean-m = new THREE.MeshBasicMaterial color: 0x112244, transparent: true, opacity: 0.8
+  new THREE.Mesh ocean-g, ocean-m
+    ..receive-shadow = true
+    .. |> scene.add
+
+  # Float
+  float-g = new THREE.CubeGeometry 5, 5, 25
+  float-m = new THREE.MeshLambertMaterial color: 0xff0000
+    ..ambient = ..color
+  float-three := new THREE.Mesh float-g, float-m
+    #  .. |> scene.add
 
   # Box
-  rig-volume := 100 * 100 * 15
   rig-g = new THREE.CubeGeometry 100, 100, 15
   rig-m = new THREE.MeshLambertMaterial color: 0x335533
     ..ambient = ..color
-  rig := new Physijs.BoxMesh rig-g, rig-m, rig-volume * 500
+  rig-three := new THREE.Mesh rig-g, rig-m
     ..cast-shadow = true
+    ..receive-shadow = true
+    ..use-quaternion = true
     ..position.z = 100
-    #..rotation.set (deg 20), (deg 20), 0
-  scene.add rig
-  rig.set-damping 0.3, 0.3
+    .. |> scene.add
 
-  # Lights
-  dir = new THREE.DirectionalLight 0xffffff
-    ..position.set -1, 0, 2
-    ..cast-shadow = true
-  scene.add dir
+  rig-volume := 100 * 100 * 15
+  rig-mass = rig-volume * 700
+  rig-rotation = new Ammo.bt-quaternion!
+    ..set-euler -pi/8.0, pi/16.0, 0
+  rig-transform = new Ammo.bt-transform!
+    ..set-identity!
+    ..set-origin (a3 0, 0, 100)
+    ..set-rotation rig-rotation
+  rig-inertia = a3 0, 0, 0
+  rig-shape = new Ammo.bt-box-shape a3 100, 100, 15
+  rig-shape.calculate-local-inertia rig-mass, rig-inertia
+  rig-motion-state = new Ammo.bt-default-motion-state rig-transform
+  rig-rb-info = new Ammo.bt-rigid-body-construction-info rig-mass, rig-motion-state, rig-shape, rig-inertia
+  rig-ammo := new Ammo.bt-rigid-body rig-rb-info
+    ..set-damping 0.2, 0.2
+  world.add-rigid-body rig-ammo
 
-  amb = new THREE.AmbientLight 0x777777
-  scene.add amb
-
-  requestAnimationFrame render
+  request-animation-frame render
 
 render = ->
-  scene.simulate ``undefined``, 1 # run physics
-  renderer.render scene, camera # render the scene
-  requestAnimationFrame render
+  update!
+  renderer.render scene, camera
+  request-animation-frame render
 
-update = (x,y,z) ->
-  rb = rig
+update = ->
+  simulate-buoyancy!
+  world.step-simulation 1/60, 5
+  update-scene!
 
-  bs = rb.geometry.bounding-sphere
-  offset = 15 - (7.5 + min(7.5, max(-7.5, rb.position.z)))
-  submerged = offset / 15
+logged = false
+simulate-buoyancy = ->
+  t = new Ammo.bt-transform!
+  rig-ammo.get-motion-state!.get-world-transform t
 
-  if submerged > 0
+  xy = 50 - 7.5
+  floats =
+    a3  xy,  xy, 0
+    a3  xy, -xy, 0
+    a3 -xy,  xy, 0
+    a3 -xy, -xy, 0
+
+  tf0 = t.op_mul floats[3]
+  float-three.position.set tf0.x!, tf0.y!, tf0.z!
+
+  for f in floats
+    tf = t.op_mul f
+
     fluid-density = 1000_kg_per_m3
-    volume-displaced = rig-volume * submerged
 
-    buoyancy = gravity
-      .clone!
-      .negate!
-      .multiply-scalar fluid-density * volume-displaced
+    height = 15
+    hheight = height/2.0
+    offset = height - (hheight + min(hheight, max(-hheight, tf.z!)))
+    volume-displaced = (rig-volume/floats.length) * (offset / hheight)
 
-    rb.apply-central-force buoyancy
+    if volume-displaced <= 0
+      continue
+
+    gravity = -9.8
+    buoyancy = a3 0, 0, -gravity * fluid-density * volume-displaced
+
+    rig-ammo.apply-force buoyancy, f
+
+
+update-scene = ->
+  t = new Ammo.bt-transform!
+  rig-ammo.get-motion-state!.get-world-transform t
+
+  pos = t.get-origin!
+  rot = t.get-rotation!
+  rig-three.position.set   pos.x!, pos.y!, pos.z!
+  rig-three.quaternion.set rot.x!, rot.y!, rot.z!, rot.w!
